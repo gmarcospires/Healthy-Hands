e699b31287ce8b0b9806ddaddde744e6fcab1a73
Marcos <40155106+ElfinLobster243@users.noreply.github.com>
2021-05-14 12:10:21 -0300

    Update

---
+++ app/src/main/java/com/example/appmaos/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.appmaos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, activity_video.class);
+        startActivity(intent);
+    }
+    
+}
+
---
+++ app/src/main/java/com/example/appmaos/WebViewClientImpl.java
@@ -0,0 +1,23 @@
+package com.example.appmaos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewClientImpl extends WebViewClient {
+    private Activity activity = null;
+    public WebViewClientImpl(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+        if(url.contains("http://192.168.2.10:5500/")) return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        activity.startActivity(intent);
+        return true;
+    }
+}
