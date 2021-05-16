ede3667bef75c78587f8e7616fc2b8e2f01eee15
Marcos <40155106+ElfinLobster243@users.noreply.github.com>
2021-05-16 19:06:22 -0300

    Versão 1.0 - Lançamento
    Agora o app está disponível para todos!

--- app/src/main/java/com/example/appmaos/activity_image.java
+++ app/src/main/java/com/example/appmaos/activity_image.java
@@ -2,13 +2,75 @@ package com.example.appmaos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class activity_image extends AppCompatActivity {
 
+    private int index;
+    ImageView imageView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image2);
+    imageView3 = findViewById(R.id.imageView3);
+
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.basico);
+       imageView3.setImageURI(uri);
+       index=0;
+    }
+
+    public void prox(View view) {
+            index++;
+            if(index>=5){
+                index=0;
+            }
+            if(index==0){
+                Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.basico);
+                imageView3.setImageURI(uri);
+            }else if (index==1){
+                Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.mobilidade);
+                imageView3.setImageURI(uri);
+            }else if (index==2){
+                Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.pressao);
+                imageView3.setImageURI(uri);
+            }else if (index==3){
+                Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.tendoes);
+                imageView3.setImageURI(uri);
+            }else{
+                Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.pulso);
+                imageView3.setImageURI(uri);
+            }
+
+    }
+    public void ant(View view) {
+        index--;
+        if(index<=-1){
+            index=4;
+        }
+        if(index==0){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.basico);
+            imageView3.setImageURI(uri);
+        }else if (index==1){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.mobilidade);
+            imageView3.setImageURI(uri);
+        }else if (index==2){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.pressao);
+            imageView3.setImageURI(uri);
+        }else if (index==3){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.tendoes);
+            imageView3.setImageURI(uri);
+        }else{
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.pulso);
+            imageView3.setImageURI(uri);
+        }
+    }
+    public void egg(View view) {
+        Intent intent = new Intent(this, egg.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/appmaos/egg.java
@@ -0,0 +1,88 @@
+package com.example.appmaos;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class egg extends AppCompatActivity {
+    private int index;
+    ImageView imageView33;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_egg);
+        imageView33 = findViewById(R.id.imageView33);
+
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggbasic);
+        imageView33.setImageURI(uri);
+        index=0;
+    }
+
+    public void prox(View view) {
+        index++;
+        if(index>=7){
+            index=0;
+        }
+        if(index==0){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggbasic);
+            imageView33.setImageURI(uri);
+        }else if (index==1){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggdemonblackflare);
+            imageView33.setImageURI(uri);
+        }else if (index==2){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggdragonflame);
+            imageView33.setImageURI(uri);
+        }else if (index==3){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggearthflash);
+            imageView33.setImageURI(uri);
+        }else if (index==4){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggicewhale);
+            imageView33.setImageURI(uri);
+        }else if (index==5){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggfireball);
+            imageView33.setImageURI(uri);
+        }else if (index==6){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggtwindragon);
+            imageView33.setImageURI(uri);
+        }else{
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggchidori);
+            imageView33.setImageURI(uri);
+        }
+
+    }
+    public void ant(View view) {
+        index--;
+        if(index<=-1){
+            index=7;
+        }
+        if(index==0){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggbasic);
+            imageView33.setImageURI(uri);
+        }else if (index==1){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggdemonblackflare);
+            imageView33.setImageURI(uri);
+        }else if (index==2){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggdragonflame);
+            imageView33.setImageURI(uri);
+        }else if (index==3){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggearthflash);
+            imageView33.setImageURI(uri);
+        }else if (index==4){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggicewhale);
+            imageView33.setImageURI(uri);
+        }else if (index==5){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggfireball);
+            imageView33.setImageURI(uri);
+        }else if (index==6){
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggtwindragon);
+            imageView33.setImageURI(uri);
+        }else{
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.drawable.eggchidori);
+            imageView33.setImageURI(uri);
+        }
+    }
+}
